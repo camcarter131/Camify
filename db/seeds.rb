21f1a70c7f9653a5111f5e6f57c5d442f25734d3
Cameron Carter <cameroncarter@utexas.edu>
2019-05-03 11:17:44 -0700

    Seed test user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'harden13', password: 'password', displayed_name: 'James Harden', email: 'the_beard@rockets.com')
