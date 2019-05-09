8ea1b7d769636259d40e79421a9880de78ab49b4
Cameron Carter <cameroncarter@utexas.edu>
2019-05-08 19:26:01 -0700

    Reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ User.create(username: 'harden13', password: 'password', displayed_name: 'James H
 
 harden = User.find_by(username: 'harden13')
 
-harden.photo.attach(io: File.open("/Users/cameroncarter/Downloads/harden.jpg"), filename:"harden.jpg")
+harden.photo.attach(io: File.open("app/assets/images/harden.jpg"), filename:"harden.jpg")
 
 ##########################################################################
 #_________________________________ARTISTS________________________________#
@@ -25,14 +25,14 @@ srv = Artist.find_by(name: 'Stevie Ray Vaughan')
 lw = Artist.find_by(name: 'Lil Wayne')
 jimi = Artist.find_by(name: 'Jimi Hendrix')
 
-srv.photo.attach(io: File.open("/Users/cameroncarter/Downloads/artist_photos/srv_artist_photo2.jpg"), filename:"srv_artist_photo2.jpg")
-srv.index_photo.attach(io: File.open("/Users/cameroncarter/Downloads/artist_photos/srv_artist_photo.jpg"), filename:"srv_artist_photo.jpg")
+srv.photo.attach(io: File.open("app/assets/images/artist_photos/srv_artist_photo2.jpg"), filename:"srv_artist_photo2.jpg")
+srv.index_photo.attach(io: File.open("app/assets/images/artist_photos/srv_artist_photo.jpg"), filename:"srv_artist_photo.jpg")
 
-lw.photo.attach(io: File.open("/Users/cameroncarter/Downloads/artist_photos/lil_wayne_artist_photo.jpeg"), filename:"lil_wayne_artist_photo.jpeg")
-lw.index_photo.attach(io: File.open("/Users/cameroncarter/Downloads/artist_photos/lw.jpg"), filename:"lw.jpg")
+lw.photo.attach(io: File.open("app/assets/images/artist_photos/lil_wayne_artist_photo.jpeg"), filename:"lil_wayne_artist_photo.jpeg")
+lw.index_photo.attach(io: File.open("app/assets/images/artist_photos/lw.jpg"), filename:"lw.jpg")
 
-jimi.photo.attach(io: File.open("/Users/cameroncarter/Downloads/artist_photos/jimi.jpg"), filename:"jimi.jpg")
-jimi.index_photo.attach(io: File.open("/Users/cameroncarter/Downloads/artist_photos/jimi_index.jpg"), filename:"jimi_index.jpg")
+jimi.photo.attach(io: File.open("app/assets/images/artist_photos/jimi.jpg"), filename:"jimi.jpg")
+jimi.index_photo.attach(io: File.open("app/assets/images/artist_photos/jimi_index.jpg"), filename:"jimi_index.jpg")
 
 
 ##########################################################################
@@ -50,10 +50,10 @@ ss = Album.find_by(name: 'Soul to Soul')
 tc = Album.find_by(name: 'Tha Carter III')
 el = Album.find_by(name: 'Electric Ladyland')
 
-tf.photo.attach(io: File.open("/Users/cameroncarter/Downloads/album_photos/tf.jpeg"), filename:"tf.jpeg")
-ss.photo.attach(io: File.open("/Users/cameroncarter/Downloads/album_photos/ss.jpg"), filename:"ss.jpg")
-tc.photo.attach(io: File.open("/Users/cameroncarter/Downloads/album_photos/tc.jpg"), filename:"tc.jpg")
-el.photo.attach(io: File.open("/Users/cameroncarter/Downloads/album_photos/el.jpg"), filename:"el.jpg")
+tf.photo.attach(io: File.open("app/assets/images/album_photos/tf.jpeg"), filename:"tf.jpeg")
+ss.photo.attach(io: File.open("app/assets/images/album_photos/ss.jpg"), filename:"ss.jpg")
+tc.photo.attach(io: File.open("app/assets/images/album_photos/tc.jpg"), filename:"tc.jpg")
+el.photo.attach(io: File.open("app/assets/images/album_photos/el.jpg"), filename:"el.jpg")
 
 ##########################################################################
 #___________________________________SONGS________________________________#
@@ -71,10 +71,10 @@ PJ = Song.find_by(name: 'Pride and Joy')
 T = Song.find_by(name: 'Testify')
 RM = Song.find_by(name: 'Rude Mood')
 
-TF.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/texas_flood.mp3"), filename:"texas_flood.mp3")
-PJ.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/PJ.mp3"), filename:"PJ.mp3")
-T.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/T.mp3"), filename:"T.mp3")
-RM.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/RM.mp3"), filename:"RM.mp3")
+TF.song_file.attach(io: File.open("app/assets/images/songs/texas_flood.mp3"), filename:"texas_flood.mp3")
+PJ.song_file.attach(io: File.open("app/assets/images/songs/PJ.mp3"), filename:"PJ.mp3")
+T.song_file.attach(io: File.open("app/assets/images/songs/T.mp3"), filename:"T.mp3")
+RM.song_file.attach(io: File.open("app/assets/images/songs/RM.mp3"), filename:"RM.mp3")
 
 ## - Soul to Soul - ##
 Song.create(name: 'Say What!', album_id: Album.find_by(name: 'Soul to Soul').id, duration: 323)
@@ -87,10 +87,10 @@ AGGL = Song.find_by(name: "Ain't Gone 'n' Give Up on Love")
 CI = Song.find_by(name: 'Change It')
 LWY = Song.find_by(name: 'Life Without You')
 
-SW.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/SW.mp3"), filename:"SW.mp3")
-AGGL.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/AGGL.mp3"), filename:"AGGL.mp3")
-CI.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/CI.mp3"), filename:"CI.mp3")
-LWY.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/LWY.mp3"), filename:"LWY.mp3")
+SW.song_file.attach(io: File.open("app/assets/images/songs/SW.mp3"), filename:"SW.mp3")
+AGGL.song_file.attach(io: File.open("app/assets/images/songs/AGGL.mp3"), filename:"AGGL.mp3")
+CI.song_file.attach(io: File.open("app/assets/images/songs/CI.mp3"), filename:"CI.mp3")
+LWY.song_file.attach(io: File.open("app/assets/images/songs/LWY.mp3"), filename:"LWY.mp3")
 
 ## - Tha Carter III - ##
 Song.create(name: '3 Peat', album_id: Album.find_by(name: 'Tha Carter III').id, duration: 144)
@@ -103,10 +103,10 @@ MC = Song.find_by(name: "Mr. Carter")
 AM = Song.find_by(name: 'A Milli')
 MO = Song.find_by(name: 'Mrs. Officer')
 
-ThreeP.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/ThreeP.mp3"), filename:"ThreeP.mp3")
-MC.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/MC.mp3"), filename:"MC.mp3")
-AM.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/AM.mp3"), filename:"AM.mp3")
-MO.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/MO.mp3"), filename:"MO.mp3")
+ThreeP.song_file.attach(io: File.open("app/assets/images/songs/ThreeP.mp3"), filename:"ThreeP.mp3")
+MC.song_file.attach(io: File.open("app/assets/images/songs/MC.mp3"), filename:"MC.mp3")
+AM.song_file.attach(io: File.open("app/assets/images/songs/AM.mp3"), filename:"AM.mp3")
+MO.song_file.attach(io: File.open("app/assets/images/songs/MO.mp3"), filename:"MO.mp3")
 
 ## - Electric Ladyland - ##
 Song.create(name: 'Voodoo Child (Slight Return)', album_id: Album.find_by(name: 'Electric Ladyland').id, duration: 244)
@@ -119,10 +119,10 @@ AAW = Song.find_by(name: "All Along the Watchtower")
 LittleWing = Song.find_by(name: 'Little Wing')
 RH = Song.find_by(name: 'Red House')
 
-VC.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/VC.mp3"), filename:"VC.mp3")
-AAW.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/AAW.mp3"), filename:"AAW.mp3")
-LittleWing.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/LittleWing.mp3"), filename:"LittleWing.mp3")
-RH.song_file.attach(io: File.open("/Users/cameroncarter/Downloads/songs/RH.mp3"), filename:"RH.mp3")
+VC.song_file.attach(io: File.open("app/assets/images/songs/VC.mp3"), filename:"VC.mp3")
+AAW.song_file.attach(io: File.open("app/assets/images/songs/AAW.mp3"), filename:"AAW.mp3")
+LittleWing.song_file.attach(io: File.open("app/assets/images/songs/LittleWing.mp3"), filename:"LittleWing.mp3")
+RH.song_file.attach(io: File.open("app/assets/images/songs/RH.mp3"), filename:"RH.mp3")
 
 
 
