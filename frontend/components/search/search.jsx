826c1aec57df0c313996445b871deda2d62eb530
Cameron Carter <cameroncarter@utexas.edu>
2019-06-13 13:52:42 -0700

    Add favicon, style search

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Search = () => {
+class Search extends React.Component {
 
-    return (
-        <div id="search-main">
-            <div className='search-input'>
-                <input className='search-input' placeholder="Start typing..."
-                    type="text"/>
-            </div>
-            <div className="search-results">
-                <div>
-                    Search Camify
+    constructor(props) {
+        super(props);
+
+    }
+
+    handleChange (e) {
+        console.log("Changed");
+    }
+
+    render () {
+
+        return (
+            <div id="search-main">
+                <div className='search-input'>
+                    <input className='search-input' placeholder="Start typing..."
+                        type="text"/>
+                </div>
+                <div className="search-results">
+                    <div id="search-text">
+                        Search Camify
+                        <p id="search-subtitle">
+                            Find your favorite songs, artists, albums, and playlists.
+                        </p>
+                    </div>
+                    
                 </div>
                 
             </div>
-            
-        </div>
-    )
+        )
+
+    }
 }
 
 export default Search;
